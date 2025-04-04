52c136cb9b8978ce215fb88b4cf4c57987598e4b
Renato Willyan Moratto Filho <80411445+RenatoMoratto@users.noreply.github.com>
2025-04-04 14:40:20 +0000

    feat: add meta tags for better SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,51 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        {/* Primary Meta Tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="author" content="Coral Jovem de Londrina" />
+        <meta
+          name="description"
+          content="Com fé e propósito, damos voz à nossa missão. A mensagem do advento a todo mundo em nossa geração."
+        />
+        <meta
+          name="keywords"
+          content="Coral, Londrina, Música, Coral Jovem, CJL, Canto, Grupo Musical"
+        />
+
+        {/* Open Graph Meta Tags (For Social Media) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Coral Jovem de Londrina" />
+        <meta
+          property="og:description"
+          content="Com fé e propósito, damos voz à nossa missão. A mensagem do advento a todo mundo em nossa geração."
+        />
+        <meta
+          property="og:image"
+          content="https://cjl-two.vercel.app/images/logo-fundo.png"
+        />
+        <meta property="og:url" content="https://cjl-two.vercel.app" />
+        <meta property="og:site_name" content="Coral Jovem de Londrina" />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Coral Jovem de Londrina" />
+        <meta
+          name="twitter:description"
+          content="Com fé e propósito, damos voz à nossa missão. A mensagem do advento a todo mundo em nossa geração."
+        />
+        <meta
+          name="twitter:image"
+          content="https://cjl-two.vercel.app/images/logo-fundo.png"
+        />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
