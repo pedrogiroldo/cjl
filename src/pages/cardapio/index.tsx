3076ddc5f444a4d57ea539a5b3ff742043076441
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-07-12 01:02:56 -0300

    add /cardapio

---
+++ src/pages/cardapio/index.tsx
@@ -0,0 +1,10 @@
+export default function Cardapio() {
+  return (
+    <div className="min-h-screen w-full bg-white flex items-center justify-center">
+      <div
+        className="aspect-[1080/1920] w-full max-w-screen-xl bg-center bg-cover"
+        style={{ backgroundImage: "url('/cardapio/bg.png')" }}
+      ></div>
+    </div>
+  );
+}
