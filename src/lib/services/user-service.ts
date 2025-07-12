db10414e32721df6fb34f6c836d26fc26514d1a1
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-07-11 22:56:12 -0300

    Add createUser method to UserService and stub API route

--- src/lib/services/user-service.ts
+++ src/lib/services/user-service.ts
@@ -8,4 +8,15 @@ export default class UserService {
     });
     return user;
   }
+
+  async createUser(email: string, password: string) {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password, // In a real application, ensure to hash the password before storing it
+      },
+      omit: { password: true },
+    });
+    return user;
+  }
 }
